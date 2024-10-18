5d71cbf3d324bc69f9a1bbaeb69ed0770269e5c8
jyothiswaroop07 <vv5382@srmist.edu.in>
2024-10-19 02:47:03 +0530

    fix():create-space-section

--- testimonials/src/app/CreateSpace/page.js
+++ testimonials/src/app/CreateSpace/page.js
@@ -1,9 +1,6 @@
 "use client";
 import React, { useState } from 'react';
 import Image from 'next/image';
-import { styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import Navbar from '../components/Navbar/Navbar';
 import LikeIcon from './LikeIcon.png';
 import { CiVideoOn } from "react-icons/ci";
@@ -11,15 +8,10 @@ import { FaPen } from "react-icons/fa";
 import { db } from '../../lib/firebase'; // Import Firestore
 import { useAuth } from '../../lib/useAuth'; // Custom hook for authentication
 import { doc, updateDoc, arrayUnion } from 'firebase/firestore'; // Import Firestore functions
-import * as React from 'react';
+
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
-import { useState } from 'react'
-import Image from 'next/image'
-import Navbar from '../components/Navbar/Navbar'
-import LikeIcon from './LikeIcon.png';
-import { FaPen } from "react-icons/fa";
 import { FaPlusCircle } from "react-icons/fa";
 
 const questionList = [
@@ -30,9 +22,11 @@ const questionList = [
 
 const Page = () => {
     const [spaceName, setSpaceName] = useState("");
-    const [spaceHeader, setSpaceHeader] = useState("");
-    const [customMessage, setCustomMessage] = useState("");
     const { user } = useAuth(); // Assuming you have a custom hook to get user info
+
+    const [questions, setQuestions] = useState(questionList);
+    const [header , setHeader] = useState('Header goes here');
+    const [customMessage , setCustomMessage] = useState('Your custom message goes here');
 
     const addSpaceToUser = async (userId, newSpace) => {
         const userRef = doc(db, 'users', userId); // Reference to the user's document
@@ -49,14 +43,14 @@ const Page = () => {
       };
 
     const handleCreateSpace = async () => {
-        if (!spaceName || !spaceHeader || !customMessage) {
+        if (!spaceName || !header || !customMessage) {
           alert("Please fill in all required fields!");
           return;
         }
       
         const newSpace = {
           name: spaceName,
-          header: spaceHeader,
+          header: header,
           message: customMessage,
           // Add any other properties you need
         };
@@ -67,66 +61,7 @@ const Page = () => {
         await addSpaceToUser(userId, newSpace); // Call the function to add the new space
       };
 
-    return (
-        <div className='w-[100vw]'>
-            <Navbar />
-            <div className='flex w-[80vw] h-[100vh] m-auto rounded-[5px] p-[40px]'>
-                <div className='flex flex-col justify-start border text-center p-5 border-dotted w-[35vw] m-2'>
-                    <div className='bg-[aquamarine] rounded-[15px] border-solid border-[1px] w-[20vw] h-[30px] ml-auto mr-auto'>
-                        <h2 className='text-center'>Live Preview - Testimonial Page</h2>
-                    </div>
-                    <div className='flex justify-center mt-10'>
-                        <Image src={LikeIcon} alt="likeIcon" width={100} height={100} className='w-[145px]' />
-                    </div>
-                    <h1 className='text-3xl mt-3'>Header goes here</h1>
-                    <p className='text-2xl mt-3'>Your custom message goes here</p>
-                    <div className='mt-5 p-[20px] flex flex-col justify-start text-left'>
-                        <h1>Questions</h1>
-                        <hr className='w-[120px] border-[blue] border-solid' />
-                        <ul>
-                            {
-                                questionList.map((eachQuestion) => {
-                                    return <li key={eachQuestion.id} className='m-3 list-[dot]'>{eachQuestion.question}</li>
-                                })
-                            }
-                        </ul>
-                    </div>
-                    <button type="button" className='flex flex-row justify-center w-[30vw] h-[40px] bg-[blue] text-[white] m-2'>
-                        <h1 className='text-center m-auto flex flex-row '><CiVideoOn size={20} style={{ margin: '2px' }} />  Record a Video</h1>
-                    </button>
-                    <button type="button" className='flex flex-row justify-center w-[30vw] h-[40px] bg-[grey] text-[white] m-2'>
-                        <h1 className='text-center m-auto flex flex-row '><FaPen size={20} style={{ margin: '2px' }} />  Record a Video</h1>
-                    </button>
-                </div>
-
-                <div className='flex flex-col justify-start border text-center p-[25px] border-solid w-[35vw] m-2'>
-                    <h1 className='text-center text-3xl font-[bold] mt-3'>Create a new space</h1>
-                    <p className='mt-5'>After the Space is created, it will generate a dedicated page for collecting testimonials.</p>
-
-                    <form className='mt-3 p-[15px] flex flex-col justify-start text-left '>
-                        <label htmlFor='name'>Space Name <span className='text-[red]'>*</span></label>
-                        <input className='border-solid border-[2px] h-[35px] w-[28vw] outline-none' id="name" type="text" onChange={(e) => setSpaceName(e.target.value)} />
-
-                        <label htmlFor='logo' className='mt-2'>Space Logo  <i className='text-[grey]'>Optional</i></label>
-                        <input type="file" accept="image/*" />
-
-                        <label htmlFor='title' className='mt-2'>Header Title <span className='text-[red]'>*</span></label>
-                        <input className='border-solid border-[2px] h-[35px] w-[28vw] outline-none' id="title" type="text" placeholder='Would you ' onChange={(e) => setSpaceHeader(e.target.value)} />
-
-                        <label htmlFor='message' className='mt-2'>Your Custom Message <span className='text-[red]'>*</span></label>
-                        <textarea className='border-solid border-[2px] h-[105px] w-[28vw] outline-none' id="message" type="text" placeholder='Write a warm message to your customers...' onChange={(e) => setCustomMessage(e.target.value)} />
-
-                        <button type="button" className='text-center w-[30vw] h-[40px] bg-[blue] text-[white] m-2' onClick={handleCreateSpace}>Create new Space</button>
-                    </form>
-                </div>
-=======
-const page = () => {
-    const [questions, setQuestions] = useState(questionList);
-    const [header , setHeader] = useState('Header goes here');
-    const [customMessage , setCustomMessage] = useState('Your custom message goes here');
-
-    // Function to handle adding a new question
-    const addQuestion = () => {
+      const addQuestion = () => {
         const newQuestion = { id: questions.length + 1, question: '' };
         setQuestions([...questions, newQuestion]);
     };
@@ -145,8 +80,10 @@ const page = () => {
         const updatedQuestions = questions.filter((question) => question.id !== id);
         setQuestions(updatedQuestions);
     };
-  return (
-    <div className='w-[100vw]' >
+
+    return (
+        
+        <div className='w-[100vw]' >
         <Navbar/>
         <div className=' flex flex-col w-[90vw] p-[10px] m-auto rounded-[5px] p-2 mb-2 lg:flex-row justify-center '>
             <div className='flex flex-col top-0 justify-start border p-[20px] border-dotted w-[85vw] lg:w-[35vw] mb-2 mt-3 text-center'>   
@@ -181,7 +118,7 @@ const page = () => {
                 <p className='mt-1'>After the Space is created, it will generate a dedicated page for collecting testimonials.</p>
                 <form className='mt-3 p-[15px] flex flex-col justify-start text-left '>
                     <label for='name' >Space Name <span className='text-[red]' >*</span></label>
-                    <input className='border-solid border-[2px] h-[35px] w-[70vw] lg:w-[28vw] outline-none ' id= "name" type="text" />
+                    <input className='border-solid border-[2px] h-[35px] w-[70vw] lg:w-[28vw] outline-none ' id= "name" type="text" onChange={(e)=>setSpaceName(e.target.value)} />
                     <label for ='logo' className='mt-2'>Space Logo  <i className='text-[grey]'>Optional</i></label>
                     <input type="file"  accept="image/" />
                     <label for='title' className='mt-2' >Header Title <span className='text-[red] ' >*</span></label>
@@ -217,11 +154,13 @@ const page = () => {
                         <FormControlLabel control={<Switch />} label="Email" />
                         <FormControlLabel control={<Switch />} label="Address" />
                     </FormGroup>
-                    <button type="button" className='text-center w-[70vw] h-[40px] bg-[blue] text-[white] m-2 lg:w-[30vw] '>Create new Space</button>
+                    <button type="button" className='text-center w-[70vw] h-[40px] bg-[blue] text-[white] m-2 lg:w-[30vw] ' onClick={handleCreateSpace}>Create new Space</button>
                 </form>
             </div>
         </div>
-    )
-}
+
+        </div>
+    
+)}
 
 export default Page;
